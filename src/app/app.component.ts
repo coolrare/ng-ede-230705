006b92d59a46b5cf11fb390540bfd1854a83061a
Will <doggy.huang@gmail.com>
2023-07-05 09:48:14 +0800

    加入 login.html 登入頁面表單版型

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'ng-ede-230705';
+
+  ngOnInit(): void {
+    document.body.className = 'bg-gradient-primary';
+  }
+  ngOnDestroy(): void {
+    document.body.className = '';
+  }
 }
