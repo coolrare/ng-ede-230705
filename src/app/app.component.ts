3fe2782192602ebb35e2a98d3e773891da8406c7
Will <doggy.huang@gmail.com>
2023-07-05 15:31:04 +0800

    完成基礎表單開發

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,33 @@ import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angula
 })
 export class AppComponent implements OnInit, OnDestroy {
   data = {
+    firstName: 'John',
+    lastName: 'Doe',
     email: 'user@example.com',
     password: '1q2w3e4R',
-    isRememberMe: true,
+    repeatPassword: '1q2w3e4R',
   };
+
+  form = this.fb.group({
+    firstName: this.fb.control('', [Validators.required]),
+    lastName: this.fb.control('', [Validators.required]),
+    email: this.fb.control('', [Validators.required]),
+    password: this.fb.control('', [Validators.required]),
+    repeatPassword: this.fb.control('', [Validators.required]),
+  });
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     document.body.className = 'bg-gradient-primary';
+
+    this.form.reset(this.data);
   }
   ngOnDestroy(): void {
+    document.body.className = '';
+  }
+
+  onSubmit() {
   }
 
 }
